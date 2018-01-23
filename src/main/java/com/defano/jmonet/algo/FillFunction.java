9343916cf7781fe3f3ca8f539728a5a839b5045e
Matt DeFano <matt@defano.com>
2018-01-22 20:07:41 -0600

    Flood-fill performance improvements

--- src/main/java/com/defano/jmonet/algo/FillFunction.java
+++ src/main/java/com/defano/jmonet/algo/FillFunction.java
@@ -1,10 +1,19 @@
 package com.defano.jmonet.algo;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * A function for filling a single pixel on the canvas.
  */
 public interface FillFunction {
-    void fill(Point p);
+
+    /**
+     * Fills a single pixel in a image with a given paint or texture.
+     *
+     * @param image The image whose pixel should be filled.
+     * @param p The point / pixel to fill
+     * @param fillPaint The paint to apply to the given pixel.
+     */
+    void fill(BufferedImage image, Point p, Paint fillPaint);
 }
--- src/main/java/com/defano/jmonet/tools/FillTool.java
+++ src/main/java/com/defano/jmonet/tools/FillTool.java
@@ -1,5 +1,7 @@
 package com.defano.jmonet.tools;
 
+import com.defano.jmonet.algo.BoundaryFunction;
+import com.defano.jmonet.algo.FillFunction;
 import com.defano.jmonet.model.PaintToolType;
 import com.defano.jmonet.tools.base.PaintTool;
 import com.defano.jmonet.algo.FloodFill;
@@ -14,6 +16,8 @@ import java.awt.image.BufferedImage;
 public class FillTool extends PaintTool {
 
     private Cursor fillCursor = new Cursor(Cursor.CROSSHAIR_CURSOR);
+    private BoundaryFunction boundaryFunction = new DefaultBoundaryFunction();
+    private FillFunction fillFunction = new DefaultFillFunction();
 
     public FillTool() {
         super(PaintToolType.FILL);
@@ -23,45 +27,27 @@ public class FillTool extends PaintTool {
     @Override
     public void mousePressed(MouseEvent e, Point imageLocation) {
 
-        BufferedImage canvasImage = getCanvas().getCanvasImage();
-        Rectangle canvasBounds = getCanvas().getBounds();
+        // Nothing to do if no fill paint is specified
+        if (getFillPaint().isPresent()) {
+            getCanvas().clearScratch();
 
-        // Nothing to do if no fill is specified
-        if (!getFillPaint().isPresent()) {
-            return;
+            FloodFill.floodFill(
+                    getCanvas(),
+                    getFillPaint().get(),
+                    imageLocation.x,
+                    imageLocation.y,
+                    fillFunction,
+                    boundaryFunction);
+
+            getCanvas().commit();
+            getCanvas().invalidateCanvas();
         }
-
-        getCanvas().clearScratch();
-
-        FloodFill.floodFill(imageLocation.x, imageLocation.y, canvasBounds, p -> {
-            int rgb = getFillPixel(p.x, p.y, getFillPaint().get());
-            getCanvas().getScratchImage().setRGB(p.x, p.y, rgb);
-        }, (Point point) -> {
-            Color canvasPixel = new Color(canvasImage.getRGB(point.x, point.y), true);
-            Color scratchPixel = new Color(getCanvas().getScratchImage().getRGB(point.x, point.y), true);
-            return canvasPixel.getAlpha() == 0 && scratchPixel.getAlpha() == 0;
-        });
-
-        getCanvas().commit();
-        getCanvas().invalidateCanvas();
     }
 
     /** {@inheritDoc} */
     @Override
     public void mouseMoved(MouseEvent e, Point imageLocation) {
         setToolCursor(getFillCursor());
-    }
-
-    private int getFillPixel(int x, int y, Paint paint) {
-
-        if (paint instanceof Color) {
-            return ((Color) paint).getRGB();
-        } else if (paint instanceof TexturePaint) {
-            BufferedImage texture = ((TexturePaint) paint).getImage();
-            return texture.getRGB(x % texture.getWidth(), y % texture.getHeight());
-        }
-
-        throw new IllegalArgumentException("Don't know how to fill with paint " + paint);
     }
 
     public Cursor getFillCursor() {
@@ -71,5 +57,64 @@ public class FillTool extends PaintTool {
     public void setFillCursor(Cursor fillCursor) {
         this.fillCursor = fillCursor;
         setToolCursor(fillCursor);
+    }
+
+    public BoundaryFunction getBoundaryFunction() {
+        return boundaryFunction;
+    }
+
+    public void setBoundaryFunction(BoundaryFunction boundaryFunction) {
+        this.boundaryFunction = boundaryFunction;
+
+        if (boundaryFunction == null) {
+            this.boundaryFunction = new DefaultBoundaryFunction();
+        }
+    }
+
+    public FillFunction getFillFunction() {
+        return fillFunction;
+    }
+
+    public void setFillFunction(FillFunction fillFunction) {
+        this.fillFunction = fillFunction;
+
+        if (fillFunction == null) {
+            this.fillFunction = new DefaultFillFunction();
+        }
+    }
+
+    /**
+     * Default boundary function; fills all fully-transparent pixels.
+     */
+    private class DefaultBoundaryFunction implements BoundaryFunction {
+        @Override
+        public boolean shouldFillPixel(BufferedImage canvas, BufferedImage scratch, Point point) {
+            Color canvasPixel = new Color(canvas.getRGB(point.x, point.y), true);
+            Color scratchPixel = new Color(scratch.getRGB(point.x, point.y), true);
+            return canvasPixel.getAlpha() == 0 && scratchPixel.getAlpha() == 0;
+        }
+    }
+
+    /**
+     * Default fill function; fills pixels with solid color or texture.
+     */
+    private class DefaultFillFunction implements FillFunction {
+        @Override
+        public void fill(BufferedImage image, Point p, Paint fillPaint) {
+            int rgb = getFillPixel(p.x, p.y, fillPaint);
+            image.setRGB(p.x, p.y, rgb);
+        }
+
+        private int getFillPixel(int x, int y, Paint paint) {
+
+            if (paint instanceof Color) {
+                return ((Color) paint).getRGB();
+            } else if (paint instanceof TexturePaint) {
+                BufferedImage texture = ((TexturePaint) paint).getImage();
+                return texture.getRGB(x % texture.getWidth(), y % texture.getHeight());
+            }
+
+            throw new IllegalArgumentException("Don't know how to fill with paint " + paint);
+        }
     }
 }
